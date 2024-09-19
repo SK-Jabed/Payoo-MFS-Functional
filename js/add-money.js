40847b4ec716b490442733e3bd518e61f9ed44f7
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-19 23:34:55 +0600

    Payoo MFS Functional Repository Added

---
+++ js/add-money.js
@@ -0,0 +1,20 @@
+/**
+ * How to get a number from an input field
+ * 1. create a variable 
+ * 2 Right side document.getElementById(id)
+ * 3. .value
+ * 4. parseFloat
+ */
+
+
+document.getElementById("add-money-btn")
+    .addEventListener("click", function(event) {
+        event.preventDefault();
+
+        // const addMoney = document.getElementById("input-add-money").value;
+        // const addMoneyNumber = parseFloat(addMoney);
+        // getInputFieldValueById();
+        // const addMoney = getInputFieldValueById();
+        // console.log("add money value", addMoney);
+
+    });
---
+++ js/utilities.js
@@ -0,0 +1,14 @@
+/**
+ * Common Shared Functions Here
+ */
+
+// function getInputFieldValueById() {
+//     const addMoney = document.getElementById("input-add-money").value;
+//     return addMoney;
+
+// }
+
+
+function getInputFieldValueById() {
+    
+}
